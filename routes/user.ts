28366bdac814f908f3daabf4200c859d28a46a56
Helio <heliob_8@hotmail.com>
2023-04-27 16:10:08 -0300

    Adicionando rotas para operações com usuários.

---
+++ routes/user.ts
@@ -0,0 +1,16 @@
+import express from "express";
+const router = express.Router();
+import { 
+    getUsersByRole,
+    getUserById,
+    updateUserById,
+    deleteUserById
+} from "../controllers/user";
+
+router.get("/:role", getUsersByRole);
+router.get("/:id", getUserById);
+router.put("/:id", updateUserById);
+router.delete("/:id", deleteUserById);
+
+// obs. da erro se tentar utilizar export default
+module.exports = router
